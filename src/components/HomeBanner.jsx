336c9c17cdd21a2847cdb62ab6140e1af9910abf
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-02-02 00:04:09 +0600

    fix footer error

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,46 +1,28 @@
-import React from 'react';
+import React from 'react'
 
 const HomeBanner = () => {
     return (
         <div>
-            <section className="bg-gray-50">
-                <div
-                    className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
-                >
-                    <div className="mx-auto max-w-xl text-center">
-                        <h1 className="text-3xl font-extrabold sm:text-5xl">
-                            Understand User Flow.
-                            <strong className="font-extrabold text-red-700 sm:block">
-                                Increase Conversion.
-                            </strong>
+            <section className='bg-slate-100'>
+                <div className='mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center'>
+                    <div className='mx-auto text-center'>
+                        <h1 className='text-3xl font-extrabold sm:text-5xl'>
+                            First, solve the problem.
+                            <strong className='font-extrabold text-primary sm:block leading-normal'>Then, write the code.</strong>
                         </h1>
-
-                        <p className="mt-4 sm:text-xl sm:leading-relaxed">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-                            tenetur fuga ducimus numquam ea!
+                        <p className='mt-4 sm:text-xl sm:leading-relaxed lg:w-8/12 mx-auto'>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+                            illo tenetur fuga ducimus numquam ea!
                         </p>
-
-                        <div className="mt-8 flex flex-wrap justify-center gap-4">
-                            <a
-                                className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                                href="/get-started"
-                            >
-                                Get Started
-                            </a>
-
-                            <a
-                                className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-                                href="/about"
-                            >
-                                Learn More
-                            </a>
+                        <div className='mt-8 flex flex-wrap justify-center gap-4'>
+                            <button className='btn btn-primary text-white rounded-md capitalize'>Get Started</button>
+                            <button className='btn btn-outline btn-primary'>Learn More</button>
                         </div>
                     </div>
                 </div>
             </section>
-
         </div>
-    );
-};
+    )
+}
 
-export default HomeBanner;
+export default HomeBanner
--- src/components/TableContent.jsx
+++ src/components/TableContent.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import SectionHeading from '../utilities/SectionHeading';
 
@@ -5,6 +6,18 @@ const TableContent = () => {
     const heading = {
         subHeading: "Team", headingOne: "All", headingTwo: "Team member", describe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
     }
+    const { data: destinations = [] } = useQuery({
+        queryKey: ['destinations'],
+        queryFn: async () => {
+            const res = await fetch(
+                'https://travel-server-woad.vercel.app/destinations',
+            )
+            const data = await res.json()
+            return data?.destinations
+        },
+    })
+
+
     return (
         <div>
             <SectionHeading heading={ heading } />
